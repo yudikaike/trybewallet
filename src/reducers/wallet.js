420dac92d6b9f59a049f83b699024db0f4427d38
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-01 12:28:00 -0300

    implementa os reducers

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { SET_CURRENCY, SET_EXPENSE } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SET_CURRENCY:
+    return { ...state, currencies: action.payload };
+  case SET_EXPENSE:
+    return { ...state, expenses: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
