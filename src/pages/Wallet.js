8031ac0533132105917c17ba69f557c5bd675d6d
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-01 13:32:54 -0300

    req-4: implementa a requisição à API do componente Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import Header from './components/Header';
+import { setCurrency } from '../actions';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currencies: [],
+    };
+  }
+
+  componentDidMount() {
+    const REQUEST_URL = 'https://economia.awesomeapi.com.br/json/all';
+    const { setCurrencyDispatch } = this.props;
+
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          currencies: Object.keys(data).filter((currencyCode) => currencyCode !== 'USDT'),
+        }, () => {
+          const { currencies } = this.state;
+          setCurrencyDispatch(currencies);
+        });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +38,12 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  setCurrencyDispatch: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrencyDispatch: (payload) => dispatch(setCurrency(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Wallet);
