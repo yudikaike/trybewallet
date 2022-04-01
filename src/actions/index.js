510ac455f777cb8c8512fc9425a9110a51af08b3
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-01 12:28:29 -0300

    implementa as actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,25 @@
 // Coloque aqui suas actions
+export const SET_USER_EMAIL = 'SET_USER_EMAIL';
+export const SET_CURRENCY = 'SET_CURRENCY';
+export const SET_EXPENSE = 'SET_EXPENSE';
+
+export const setUserEmail = (payload) => (
+  {
+    type: SET_USER_EMAIL,
+    payload,
+  }
+);
+
+export const setCurrency = (payload) => (
+  {
+    type: SET_CURRENCY,
+    payload,
+  }
+);
+
+export const setExpense = (payload) => (
+  {
+    type: SET_EXPENSE,
+    payload,
+  }
+);
