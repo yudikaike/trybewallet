c972bb2ba765e55f84b680c27d1cdb2ebed55cca
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-04 12:38:05 -0300

    req-10: implementa botão para entrar no modo de edição da despesa

--- src/pages/components/ExpensesTable.js
+++ src/pages/components/ExpensesTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { deleteExpense } from '../../actions';
+import { deleteExpense, editTargetId } from '../../actions';
 
 class ExpensesTable extends Component {
   constructor() {
@@ -9,12 +9,19 @@ class ExpensesTable extends Component {
     this.findExchangeRate = this.findExchangeRate.bind(this);
     this.renderTableData = this.renderTableData.bind(this);
     this.onClickDelete = this.onClickDelete.bind(this);
+    this.onClickEdit = this.onClickEdit.bind(this);
   }
 
   onClickDelete({ target: { value } }) {
     const { deleteExpenseDispatch } = this.props;
 
     deleteExpenseDispatch(parseInt(value, 10));
+  }
+
+  onClickEdit({ target: { value } }) {
+    const { editIdDispatch } = this.props;
+
+    editIdDispatch(parseInt(value, 10));
   }
 
   findExchangeRate(expense) {
@@ -28,6 +35,7 @@ class ExpensesTable extends Component {
     const { name, ask } = this.findExchangeRate(expense);
 
     const parsedValue = parseFloat(value).toFixed(2);
+    const parsedName = name.split('/')[0];
     const parsedAsk = parseFloat(ask).toFixed(2);
     const parsedConversion = (parseFloat(value) * parseFloat(ask)).toFixed(2);
 
@@ -37,12 +45,19 @@ class ExpensesTable extends Component {
         <td>{ tag }</td>
         <td>{ method }</td>
         <td>{ parsedValue }</td>
-        <td>{ name }</td>
+        <td>{ parsedName }</td>
         <td>{ parsedAsk }</td>
         <td>{ parsedConversion }</td>
         <td>Real</td>
         <td>
-          <button type="button">Editar</button>
+          <button
+            type="button"
+            data-testid="edit-btn"
+            value={ id }
+            onClick={ this.onClickEdit }
+          >
+            Editar
+          </button>
           <button
             type="button"
             data-testid="delete-btn"
@@ -87,6 +102,7 @@ ExpensesTable.propTypes = {
     PropTypes.object,
   ).isRequired,
   deleteExpenseDispatch: PropTypes.func.isRequired,
+  editIdDispatch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -95,6 +111,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   deleteExpenseDispatch: (payload) => dispatch(deleteExpense(payload)),
+  editIdDispatch: (payload) => dispatch(editTargetId(payload)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpensesTable);
