160f17e4a1cc879ffd47d6afae43506836a9b84c
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-04 12:36:27 -0300

    req-10: implementa lógica ao reducer de editar uma despesa

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,9 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { SET_CURRENCY, SET_EXPENSE, DELETE_EXPENSE } from '../actions';
+import {
+  SET_CURRENCY,
+  SET_EXPENSE,
+  DELETE_EXPENSE,
+  EDIT_TARGET_ID,
+  EDIT_EXPENSE,
+} from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  editId: '',
 };
 
 function wallet(state = INITIAL_STATE, action) {
@@ -15,6 +22,16 @@ function wallet(state = INITIAL_STATE, action) {
   case DELETE_EXPENSE:
     return { ...state,
       expenses: state.expenses.filter(({ id }) => id !== action.payload) };
+  case EDIT_TARGET_ID:
+    return { ...state, editId: action.payload };
+  case EDIT_EXPENSE:
+    return { ...state,
+      expenses: state.expenses.map((expense) => {
+        if (expense.id !== action.payload.id) {
+          return expense;
+        }
+        return action.payload;
+      }) };
   default:
     return state;
   }
