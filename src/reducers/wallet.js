ff2f61c193fba49e3a7f7e647faccb0f73ef8ec1
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-04 10:51:55 -0300

    req-9: implementa lógica ao reducer wallet para atualizar o estado

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,5 +1,5 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { SET_CURRENCY, SET_EXPENSE } from '../actions';
+import { SET_CURRENCY, SET_EXPENSE, DELETE_EXPENSE } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -12,6 +12,9 @@ function wallet(state = INITIAL_STATE, action) {
     return { ...state, currencies: action.payload };
   case SET_EXPENSE:
     return { ...state, expenses: [...state.expenses, action.payload] };
+  case DELETE_EXPENSE:
+    return { ...state,
+      expenses: state.expenses.filter(({ id }) => id !== action.payload) };
   default:
     return state;
   }
