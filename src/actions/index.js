0e93d008095d0f69986e4563bd8a3cb8cd9cec52
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-04 12:35:42 -0300

    req-10: implementa action de editar uma despesa

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ export const SET_USER_EMAIL = 'SET_USER_EMAIL';
 export const SET_CURRENCY = 'SET_CURRENCY';
 export const SET_EXPENSE = 'SET_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+export const EDIT_TARGET_ID = 'EDIT_TARGET_ID';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
 
 export const setUserEmail = (payload) => (
   {
@@ -28,6 +30,20 @@ export const setExpense = (payload) => (
 export const deleteExpense = (payload) => (
   {
     type: DELETE_EXPENSE,
+    payload,
+  }
+);
+
+export const editTargetId = (payload) => (
+  {
+    type: EDIT_TARGET_ID,
+    payload,
+  }
+);
+
+export const editExpense = (payload) => (
+  {
+    type: EDIT_EXPENSE,
     payload,
   }
 );
