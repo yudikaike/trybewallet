16aff09bbfc121e7ce9a1f84e4eeaaac7a497901
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-04 09:12:59 -0300

    req-6: atualiza a lógica do reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -11,7 +11,7 @@ function wallet(state = INITIAL_STATE, action) {
   case SET_CURRENCY:
     return { ...state, currencies: action.payload };
   case SET_EXPENSE:
-    return { ...state, expenses: action.payload };
+    return { ...state, expenses: [...state.expenses, action.payload] };
   default:
     return state;
   }
